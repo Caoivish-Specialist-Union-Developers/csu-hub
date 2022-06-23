1b0f6f58cc9e6bc282c08dbb1e31afd008059f5c
ianfights <ianfights@gmail.com>
2022-06-23 15:08:59 -0400

    Finished basic boilerplate like the router and the landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+const LandingPage = () => {
+    return (
+        <div className="landingPage" style={{ textAlign: "center" }}>
+            <h1>Lorem Ipsum</h1>
+            <p>
+                Lorem ipsum dolor sit amet, dolor in eiusmod velit do nostrud nulla occaecat tempor veniam id laboris minim fugiat occaecat do ipsum Lorem minim quis tempor tempor duis ea officia nisi aliqua eiusmod esse ut commodo quis ipsum aute commodo cillum consectetur id exercitation tempor eu et aliquip veniam minim laborum esse velit ex exercitation dolor ea officia exercitation aute nisi culpa ullamco in tempor cillum enim est nostrud ipsum enim esse mollit irure sint sunt adipisicing velit est officia magna do anim do magna officia dolore consequat fugiat eiusmod reprehenderit Lorem consequat dolor aliqua sunt occaecat consequat ullamco reprehenderit officia ea ullamco commodo aliquip
+            </p>
+        </div>
+    );
+};
+
+export default LandingPage;
